08666f2b9682927d374d302e7eafe9f598057aa6
Homaira123 <108037582+Homaira123@users.noreply.github.com>
2022-10-03 18:54:43 -0700

    Create parks.py

---
+++ parks.py
@@ -0,0 +1,65 @@
+from flask import render_template,redirect,request,session
+from flask_app import app
+from flask_app.models.user import User
+from flask_app.models.park import Park
+
+
+
+@app.route("/dashboard")
+def user_dashboard():
+    user_data={
+        'id':session['user_id']
+    }
+    user=User.get_user_by_id(user_data)
+    parks=Park.get_all()
+    return render_template("dashboard.html", user=user,parks=parks)
+
+@app.route("/parks/new")
+def new_park_form():
+    user_data={
+        'id': session['user_id']
+    }
+    user= User.get_user_by_id(user_data)
+    return render_template("create_parks.html",user=user)
+    
+@app.route("/parks/create",methods=['POST'])
+def create_park():
+    if not Park.validate_create(request.form):
+        return redirect("/parks/new")
+    Park.create(request.form)
+    return redirect("/dashboard")
+
+@app.route("/show/<int:id>")
+def show_park(id):
+    user_data={
+        'id': session['user_id']
+    }
+    user = User.get_user_by_id(user_data)
+    park_data={
+        "id":id
+
+    }
+    park= Park.get_one(park_data)
+    return render_template('show_park.html',park=park, user=user )
+
+@app.route("/edit/<int:id>")
+def show_edit_form(id):
+    user_data ={
+        'id': session['user_id']
+    }
+    user = User.get_user_by_id(user_data)
+    park_data ={
+        'id':id
+    }
+    park = Park.get_one(park_data)
+    return render_template("edit_park.html",user=user,park=park)
+
+@app.route("/parks/<int:id>/update", methods=['POST'])
+def update_park(id):
+    Park.update(request.form)
+    return redirect('/dashboard')
+
+@app.route("/parks/<int:id>/delete", methods = ['POST'])
+def delete(id):
+    Park.delete(request.form)
+    return redirect("/dashboard")
